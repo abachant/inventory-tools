e4d9bd8ec399a44f260392d846c61fe7ded808e6
abachant <aubreybachant@gmail.com>
2025-01-09 11:29:18 -0500

    Add basic unit test for combine_duplicate_entries

--- test_inventorytools.py
+++ test_inventorytools.py
@@ -1,2 +1,22 @@
 import unittest
+import pandas as pd
+from inventorytools import combine_dublicate_entries
 
+class TestCombineDuplicateEntries(unittest.TestCase):
+    uncombined_df = pd.DataFrame({
+    "Brand": ["Straumann", "Straumann", "Straumann"],
+    "Reference": ["21.2508", "21.2508", "21.3308"],
+    "Quantity": [2, 4, 30]
+    })
+        
+    combined_df = pd.DataFrame({
+    "Brand": ["Straumann", "Straumann"],
+    "Reference": ["21.2508", "21.3308"],
+    "Quantity": [6, 30]
+    })
+    def test_combine_duplicate_entries(self):
+        output_df = combine_dublicate_entries(self.uncombined_df)
+        pd.testing.assert_frame_equal(output_df, self.combined_df, obj="Output DataFrame does not match the expected combined DataFrame.")
+
+if __name__ == "__main__":
+    unittest.main()
