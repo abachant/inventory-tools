dfaedad20116df1e8449beb30246c11563dc3179
abachant <aubreybachant@gmail.com>
2025-01-09 11:23:25 -0500

    Add basic function for combining duplicate entries

---
+++ inventorytools.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def combine_dublicate_entries(df):
+    combined_df = df.groupby(['Brand', 'Reference'], as_index=False)['Quantity'].sum()
+    
+    combined_df = combined_df.sort_values(by=['Brand', 'Reference']).reset_index(drop=True)
+    return combined_df
