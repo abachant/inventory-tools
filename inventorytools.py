183f9fd845888199598e546a99f571f73bb1cfa3
abachant <aubreybachant@gmail.com>
2025-01-21 21:50:24 -0500

    Add function to simplify brand names to one word

--- inventorytools.py
+++ inventorytools.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
+
 def combine_dublicate_entries(df):
-    combined_df = df.groupby(['Brand', 'Reference'], as_index=False)['Quantity'].sum()
-    
-    combined_df = combined_df.sort_values(by=['Brand', 'Reference']).reset_index(drop=True)
+    combined_df = df.groupby(['Brand', 'Reference'], as_index=False)[
+        'Quantity'].sum()
+
+    combined_df = combined_df.sort_values(
+        by=['Brand', 'Reference']).reset_index(drop=True)
     return combined_df
+
+
+def simplify_brand_names(df):
+    df["Brand"] = df["Brand"].str.split(" ").str[0]
+    return df
